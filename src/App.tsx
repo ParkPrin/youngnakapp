77b3f695325222117406e8f3ecc3913919413d62
박종훈 <m05214@naver.com>
2022-10-06 16:09:38 +0900

    Feat: State 생명주기까지 학습진행함
    State 생명주기까지 학습진행함

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Person } from "./user/Person";
 
-function App() {
+const personInfo: Function = (person: Person) => (
+  <p>
+    이름: {person.getName()}, 나이: {person.getAge()}
+  </p>
+);
+
+const clock: Function = (date: Date) => <h2>{date.toLocaleTimeString()}.</h2>;
+
+function App(props: any) {
+  const person: Person = props.person;
+  const [date, setDate] = useState<Date>(new Date());
+
+  useEffect(() => {
+    setDate(new Date());
+    document.title = `시간 ${date} 입니다`;
+  });
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +26,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        {personInfo(person)}
+        {clock(date)}
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Person } from "./user/Person";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const appFuction = () => {
+  const person: Person = new Person(`박종훈`, 35);
+  const element = (
+    <React.StrictMode>
+      <App person={person} />
+    </React.StrictMode>
+  );
+  root.render(element);
+};
+
+appFuction();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
