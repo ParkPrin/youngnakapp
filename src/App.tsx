b00eb403a7ffde5dbcbeb6e2434d62d8995c7724
박종훈 <m05214@naver.com>
2022-10-15 22:52:10 +0900

    Feat: 클래스 컴포넌트에서 함수형 컴포넌트로 교체
    클래스 컴포넌트에서 함수형 컴포넌트로 교체

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,19 @@
-import React, {ChangeEvent, Component} from "react";
+import React, {ChangeEvent, useState} from "react";
 import "./App.css";
 
-interface TodoData {
+type TodoData = {
     id: number;
     title: string;
     completed: boolean;
-}
+};
 
-type State = {
-    todoDatas: TodoData[];
-    value: string;
-    keyIndex: number;
-}
 
-export default class App extends Component<any, State>{
-  state: State = {
-      todoDatas: [
-          {
-              id: 1,
-              title: "공부하기",
-              completed: true,
-          },
-          {
-              id: 2,
-              title: "청소하기",
-              completed: false,
-          }
-      ],
-      value: '',
-      keyIndex: 2
-  }
-  btnStyle = {
+export default function App():JSX.Element {
+  const [todoDatas, setTodoDatas] = useState<TodoData[]>([]);
+  const [value, setValue] = useState<string>("");
+  const [keyIndex, setKeyIndex] = useState<number>(0);
+
+  const btnStyle = {
       color: "#fff",
       border: "none",
       padding: "5px 9px",
@@ -38,7 +21,7 @@ export default class App extends Component<any, State>{
       cursor: "pointer",
       float: "right" as "right"
   }
-  getStyle = (completed: boolean): any => {
+  const getStyle = (completed: boolean): any => {
       return {
           marginTop: "15px",
           padding: "10px",
@@ -47,65 +30,58 @@ export default class App extends Component<any, State>{
       }
   }
 
-  deleteItem = (id: number): void => {
-      const newTodoData = this.state.todoDatas.filter((data: TodoData) => data.id !== id)
-      this.setState({
-          todoDatas: newTodoData
-      })
+  const deleteItem = (id: number): void => {
+      const newTodoData = todoDatas.filter((data: TodoData) => data.id !== id)
+      setTodoDatas(newTodoData)
   }
-  addItem = (e: React.MouseEvent<HTMLInputElement>): void => {
+  const addItem = (e: React.MouseEvent<HTMLInputElement>): void => {
       e.preventDefault();
-      const keyIndex: number = this.state.keyIndex + 1;
+      const key: number = keyIndex + 1;
 
       const newTodoItem: TodoData = {
-        id: keyIndex,
-        title: this.state.value,
+        id: key,
+        title: value,
         completed: false,
-      }
-      this.setState({
-          todoDatas: [...this.state.todoDatas, newTodoItem],
-          keyIndex: keyIndex,
-          value: ''
-      })
+      };
+      setTodoDatas(prev =>
+          [...prev, newTodoItem]);
+      setKeyIndex(key);
+      setValue("")
   }
 
-  inputTitle = (e:ChangeEvent<HTMLInputElement>): void => {
-      this.setState({
-          value: e.target.value
-      })
+  const inputTitle = (e:ChangeEvent<HTMLInputElement>): void => {
+      setValue(e.target.value);
   }
-  changeCheckbox = (id: number): void => {
-      const changeTodoDatas = this.state.todoDatas.map((item) => {
+  const changeCheckbox = (id: number): void => {
+      const changeTodoDatas = todoDatas.map((item) => {
           if (item.id === id){
               item.completed = !item.completed;
           }
           return item;
       });
-      this.setState({
-          todoDatas: changeTodoDatas
-      })
+      setTodoDatas(changeTodoDatas);
   }
 
-  render() {
+
     return (
         <div className="container">
           <div className="todoBlock">
               <div className="title">
                   <h1>할 일 목록</h1>
               </div>
-              {this.state.todoDatas.map((data:TodoData) => (
-                  <div key={data.id} style={this.getStyle(data.completed)}>
-                      <input type="checkbox" defaultChecked={data.completed} onChange={() => this.changeCheckbox(data.id)}/>
+              {todoDatas.map((data:TodoData) => (
+                  <div key={data.id} style={getStyle(data.completed)}>
+                      <input type="checkbox" defaultChecked={data.completed} onChange={() => changeCheckbox(data.id)}/>
                       {data.title}
-                      <button style={this.btnStyle} onClick={() => this.deleteItem(data.id)}>x</button>
+                      <button style={btnStyle} onClick={() => deleteItem(data.id)}>x</button>
                   </div>
               ))}
               <form style={{ display: 'flex'}}>
-                  <input type="text" name="value" value={this.state.value} style={{ flex: '10', padding: '5px'}} onChange={(e) => this.inputTitle(e)}/>
-                  <input type="submit" value="입력" className="btn" style={{flex: '1'}} onClick={(e) => this.addItem(e)}/>
+                  <input type="text" name="value" value={value} style={{ flex: '10', padding: '5px'}} onChange={(e) => inputTitle(e)}/>
+                  <input type="submit" value="입력" className="btn" style={{flex: '1'}} onClick={(e) => addItem(e)}/>
               </form>
           </div>
         </div>
     )
-  }
+
 }
