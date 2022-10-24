9afe34e754858ba41a9ed337e2d41c937584dcfd
박종훈 <m05214@naver.com>
2022-10-24 16:55:05 +0900

    Feat: 할 일 목록 모두 수정하기 기능 추가
    할 일 목록 모두 수정하기 기능 추가

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import {TodoData} from "../type/TodoData";
 import {DraggableProvided, DraggableStateSnapshot} from "react-beautiful-dnd";
 
@@ -8,9 +8,24 @@ type Props = {
   snapshot:DraggableStateSnapshot;
   changeCheckbox: Function,
   deleteItem: Function,
+  changeTodoDatas: Function,
 }
 
-const Item = React.memo(({data, provided, snapshot, changeCheckbox, deleteItem}: Props) => {
+const Item = React.memo(({data, provided, snapshot, changeCheckbox, deleteItem, changeTodoDatas}: Props) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(data.title);
+
+  const editingItem = ():void => {
+    setIsEditing(!isEditing);
+    if (isEditing) {
+      const editData:TodoData = data;
+      editData.title = editedTitle;
+      changeTodoDatas(editData);
+    }
+  }
+  const changeEditedTitle = (e:ChangeEvent<HTMLInputElement>):void => {
+    setEditedTitle(e.target.value);
+  }
   return (
     <div key={data.id} {...provided.draggableProps} ref={provided.innerRef} {...provided.dragHandleProps} className={`${snapshot.isDragging ? "bg-gray-400": "bg-gray-100"} flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rounded`}>
       <div className="items-center">
@@ -18,13 +33,23 @@ const Item = React.memo(({data, provided, snapshot, changeCheckbox, deleteItem}:
             type="checkbox"
             defaultChecked={data.completed}
             onChange={() => changeCheckbox(data.id)}/>
-          <span className={data.completed ? "line-through" : undefined} >
-            {data.title}
+          <span>
+            {
+              !isEditing ?
+                <span className="ml-2">{data.title}</span>
+              : <input type="text" name="editedTitle"
+                      className="ml-2 px-3 py-2 mr-4 text-gray-300 border rounded text-slate-800"
+                      placeholder="해야 할 일을 입력하세요"
+                      value={editedTitle}
+                      onChange={changeEditedTitle}
+                />
+            }
           </span>
       </div>
-        <div className="items-center">
-          <button onClick={() => deleteItem(data.id)} className="px-4 py-2 float-right">x</button>
-        </div>
+      <div className="items-center">
+        <button onClick={() => deleteItem(data.id)} className="px-4 py-2 float-right">x</button>
+        <button onClick={() => editingItem()} className="px-4 py-2 float-right">{isEditing ? "save" : "edit"}</button>
+      </div>
     </div>
   );
 });
--- src/components/Lists.tsx
+++ src/components/Lists.tsx
@@ -18,14 +18,13 @@ type Props = {
 const Lists = React.memo((
     {todoDatas, setTodoDatas}: Props,
   ):JSX.Element => {
-  const handleEnd = useCallback((result:DropResult) => {
+  const handleEnd = (result:DropResult) => {
     if(!result.destination) return;
     const newTodoDatas = todoDatas;
-
-    const [reorderItem] = newTodoDatas.splice((result.source.index, 1));
+    const [reorderItem] = todoDatas.splice(result.source.index, 1);
     newTodoDatas.splice(result.destination.index, 0, reorderItem);
     setTodoDatas(newTodoDatas)
-  }, [todoDatas, setTodoDatas]);
+  };
   const changeCheckbox = useCallback((id: number): void => {
     const changeTodoDatas = todoDatas.map((item) => {
       if (item.id === id){
@@ -34,9 +33,16 @@ const Lists = React.memo((
       return item;
     });
     setTodoDatas(changeTodoDatas);
-  }, [todoDatas, setTodoDatas])
+  }, [todoDatas, setTodoDatas]);
+  const changeTodoDatas = useCallback((todoData: TodoData): void => {
+    const resultTodoDatas:TodoData[] = todoDatas.map((item:TodoData) => {
+      return item.id === todoData.id ? todoData : item;
+    });
+    setTodoDatas(resultTodoDatas);
+  }, [todoDatas, setTodoDatas]);
 
   const deleteItem = (id: number): void => {
+    console.log(`deleteItem: ${id}`);
     const newTodoData = todoDatas.filter((data: TodoData) => data.id !== id)
     setTodoDatas(newTodoData)
   }
@@ -49,7 +55,7 @@ const Lists = React.memo((
                   {todoDatas.map((data:TodoData, index: number) => (
                     <Draggable draggableId={data.id.toString()} key={data.id} index={index}>
                       {(provided:DraggableProvided, snapshot:DraggableStateSnapshot) => (
-                        <Item data={data} provided={provided} snapshot={snapshot} changeCheckbox={changeCheckbox} deleteItem={deleteItem} />
+                        <Item data={data} provided={provided} snapshot={snapshot} changeCheckbox={changeCheckbox} deleteItem={deleteItem} changeTodoDatas={changeTodoDatas} />
                       )}
                     </Draggable>
                 ))}
