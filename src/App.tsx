9bd0af6be2dc52a21e5daa79871187b63f134d0a
박종훈 <m05214@naver.com>
2022-10-24 14:42:17 +0900

    Feat: 할 일 목록 모두 삭제하기 기능 추가
    할 일 목록 모두 삭제하기 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,15 @@ import Form from "./components/Form";
 export default function App():JSX.Element {
   const [todoDatas, setTodoDatas] = useState<TodoData[]>([]);
   const [keyIndex, setKeyIndex] = useState<number>(0);
+  const handleRemoveClick = ():void => {
+    setTodoDatas([]);
+  }
     return (
       <div className="flex items-center justify-center w-screen h-screen bg-blue-200">
         <div className="w-full p-6 m-4 bg-white shadow lg:w-3/4 lg:max-w-lg">
           <div className="flex justify-between mb-3">
             <h1>할 일 목록</h1>
+            <button onClick={handleRemoveClick}>Delete All</button>
           </div>
           <Lists todoDatas={todoDatas} setTodoDatas={setTodoDatas}  />
           <Form
