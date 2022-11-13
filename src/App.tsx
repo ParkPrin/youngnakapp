1c86ae569b9a2426bbc3bdc44bdd26ec9c2aca54
박종훈 <m05214@naver.com>
2022-11-14 07:21:12 +0900

    Feat: 이미지배너 작업
    이미지배너 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Nav from "./components/Nav";
+import Banner from "./components/Banner";
 
 const App = ():JSX.Element => {
     return (
       <div className="app">
         <Nav />
+        <Banner />
       </div>
     );
 }
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import requests from "../api/requests";
+import axios from "../api/axios";
+import "./Banner.css"
 
 const Banner = ():JSX.Element => {
-  return (
-    <div>
+  const [movie, setMovie]: any = useState({});
+  useEffect(() => {
+    fetchData();
+  }, [])
 
-    </div>
+  const fetchData = async () => {
+
+    // 현재 상영중인 영화 정보를 가져오(여러 영화
+
+    const request = await axios.get(requests.fetchNowPlaying)
+    const movieId = request.data.results[
+      Math.floor(Math.random() * request.data.results.length)
+    ].id;
+    const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
+      params: { append_to_reponse: "videos"}
+    });
+    console.log(movieDetail);
+    setMovie(movieDetail)
+  }
+  const truncate:Function = (str:any, n:number):string => {
+    return str?.length > n ? str.substr(0, n-1) + "..." : str;
+  };
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+        backgroundPosition: "top center",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie.title || movie.name || movie.original_name}
+        </h1>
+
+        <div className="banner__buttons">
+          <button
+            className="banner__button play"
+          >
+            Play
+          </button>
+          <button className="banner__button info">More Information</button>
+        </div>
+
+        <h1 className="banner__description">
+          {truncate(movie.overview, 100)}
+        </h1>
+      </div>
+      <div className="banner--fadeBottom" />
+    </header>
   );
 }
 
